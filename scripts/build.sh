eecfac4a97cb6b00bd0258db6b1beb878a027daf
Azriel Hoh <azriel91@gmail.com>
2020-02-14 18:12:33 +1300

    Don't use cache for nightly build.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
 docker build --build-arg CHANNEL=stable -t amethystrs/builder-linux:stable linux
-docker build --build-arg CHANNEL=nightly -t amethystrs/builder-linux:nightly linux
+docker build --no-cache --build-arg CHANNEL=nightly -t amethystrs/builder-linux:nightly linux
